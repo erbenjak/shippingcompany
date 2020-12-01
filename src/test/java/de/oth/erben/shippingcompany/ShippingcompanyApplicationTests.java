4de6ef53a78fd2d9435e331a454ee5fa8731c31a
erbenjak <jakobus.erben@gmail.com>
2020-12-01 17:25:59 +0100

    Initial Project Setup

---
+++ src/test/java/de/oth/erben/shippingcompany/ShippingcompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package de.oth.erben.shippingcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingcompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
